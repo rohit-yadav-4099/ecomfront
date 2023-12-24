03b8be24be5c3623a8b68cf0a72dbd6d0e13c8f5
Rohit Yadav <131008606+rohit-yadav-4099@users.noreply.github.com>
2023-12-24 20:07:06 +0530

    Update Laptop.js

--- src/Component/Laptop.js
+++ src/Component/Laptop.js
@@ -31,7 +31,7 @@ function Laptop() {
                 {data.filter((item) => item.category === "laptop").map((item, index) => {
                     const {
                         id = item.id,
-                        title = item.title,
+                        name = item.name,
                         image = item.image,
                         price = parseInt(item.price),
                     } = item;
@@ -40,17 +40,25 @@ function Laptop() {
                             <div key={index} className="underdiv_premium">
                                 <NavLink to={`/details/${item.id}`}>    <img src={item.image} alt="Not Found" className="all_images_premium" /></NavLink>
                                 <div className="underdiv_premium_two">
-                                    <span>{item.title}laptop</span>
+                                    <span>{item.name}laptop</span>
                                     <span className="price_premium">₹:{item.price}.00</span>
                                     {/* <h2 className="title_premium">{item.title}</h2> */}
-                                    <NavLink to={`/addcard/${item.id}`}>  <button onClick={() => dispatch(addtoCart({ id, price, image, title }))} className="btnaddcard_premium">add to cart</button></NavLink>
+                                    {/* <NavLink to={`/addcard/${item.id}`}>  <button onClick={() => dispatch(addtoCart({ id, price, image, title }))} className="btnaddcard_premium">add to cart</button></NavLink> */}
+                                    <button
+                                        className="btnaddcard_home"
+                                        onClick={() =>
+                                            dispatch(addtoCart({ id, price, image, name }))
+                                        }
+                                    >
+                                        add to cart
+                                    </button>
                                 </div>
                             </div>
                         </>
                     )
                 })}
             </div>
-         <Footer/>
+            <Footer />
         </>
     )
 }
