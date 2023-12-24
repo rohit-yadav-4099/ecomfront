848a05470a8e09b0debd27a4ed1db56ce31eeee0
Rohit Yadav <131008606+rohit-yadav-4099@users.noreply.github.com>
2023-12-24 20:03:24 +0530

    Update Detail.js

--- src/Component/Detail.js
+++ src/Component/Detail.js
@@ -1,7 +1,6 @@
 import { NavLink, useNavigate, useParams } from "react-router-dom"
 import { useEffect, useState } from "react"
 import '../Css/Rout.css'
-
 import axios from "axios";
 import { useDispatch } from "react-redux";
 import { addtoCart } from "../Redux/Slice";
@@ -11,34 +10,26 @@ function DetailsCompo() {
     const [data, setData] = useState([])
     const Navi = useNavigate();
     const dispatch = useDispatch();
-
-
     const { id } = useParams()
     const newid = parseInt(id)
 
-
     useEffect(() => {
-
-
         axios.get("https://ecom-sk46.onrender.com/api/getdata")
             .then((res) => setData(res.data))
             .catch((err) => {
                 console.log("error")
             })
-
-
-
     }, [id])
+
     return (
         <>
-
-
             <div className="details_wrrraper">
                 {data.filter((item) => item.id === newid).map((item, index) => {
                     const {
                         id = item.id,
                         name = item.name,
                         image = item.image,
+                        rating = item.rating,
                         price = parseInt(item.price),
                     } = item;
                     return (
@@ -46,16 +37,16 @@ function DetailsCompo() {
                             <img src={item.image} alt="Not Found" className="all_images_details" />
                             <div className="underdiv_details">
                                 <h3 className="title_details">{item.name}</h3>
-                                <p className="heading_details">{item.name}</p>
-                                <span className="price_details">₹:{item.price}.00</span>
-
-
-
-                                <p className="moredetails"> <h1>Discreption</h1>{item.para}</p>
-                                <NavLink to={`/addcard/${item.id}`}><button className="btnaddcard_details" onClick={() =>
-                                    dispatch(addtoCart({ id, price, image, name }))
-                                }>add to card</button></NavLink></div>
-
+                                <h3 className="price_details">₹:{item.price}.00</h3>
+                                <p className="moredetails"> <h2>Discreption</h2>
+                                    {item.name}<br />
+                                    {item.subCategory}<br />
+                                    {item.brand}<br />
+                                    {item.rating}</p>
+                                <button className="btnaddcard_details" onClick={() =>
+                                    dispatch(addtoCart({ id, price, image, name, rating }))
+                                }>add to card</button>
+                            </div>
                         </div>
                     )
                 })}
