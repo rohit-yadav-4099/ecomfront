0cdeed21152fac8da013dc51f0cc0cde3c7cb52b
Rohit Yadav <131008606+rohit-yadav-4099@users.noreply.github.com>
2023-12-24 19:48:29 +0530

    Update AddtoCart.js

--- src/Redux/AddtoCart.js
+++ src/Redux/AddtoCart.js
@@ -4,6 +4,7 @@ import "../Css/AddCart.css";
 import { RemoveItem, IncreaseQuantity, DecreaseQuantity } from "../Redux/Slice";
 import { useNavigate } from "react-router-dom";
 import { loadStripe } from "@stripe/stripe-js";
+import Footer from "../Component/Footer1";
 
 const Cart = () => {
     const Navi = useNavigate();
@@ -19,23 +20,22 @@ const Cart = () => {
         dispatch(DecreaseQuantity({ id }));
     };
 
+    // payment integrate...
+
     const handleBuy = async () => {
-        const stripe = await loadStripe(
-            "pk_test_51OFfDcSFuYPQ8NkkiTz5ftHV4vxynOQ1qBrVphWeG7zUdAdr0biKAWFhLRGZfedGDI96o3QL1qhPfv6M8J5nkwrr00E5cXKqKc"
-        );
+        const stripe = await loadStripe("pk_test_51OQ699SDqC8mEPn5Q8XJZPCPbWDs3QdmS0iRgQP6uGNG3AHBgIIRiBrDhW6gB1QHC3C9GnJdLF7jW4sgEDA8zTJH00W163lIIE");
         const body = {
             products: data,
         };
         const headers = {
             "Content-Type": "application/json",
         };
-        const response = await fetch("https://ecom-sk46.onrender.com/checkout",
+        const response = await fetch("https://localhost:7800/api/create-checkout-session",
             {
                 method: "POST",
                 headers: headers,
                 body: JSON.stringify(body),
-            }
-        );
+            });
         const session = await response.json();
         const result = stripe.redirectToCheckout({
             sessionId: session.id,
@@ -56,12 +56,12 @@ const Cart = () => {
                                 </div>
                                 <div className="cart_details">
                                     <div>
-                                        <h2 className="cartprice">
+                                        <h3>{item.name}</h3>
+                                        <span>{item.subCategory}</span>
+                                        <h3 className="cartprice">
                                             {"Rs " + item.price * item.quantity}.00
-                                            <p>
-                                                <span>Rating</span>⭐⭐⭐⭐⭐
-                                            </p>
-                                        </h2>
+
+                                        </h3>
 
                                         <div className="addsubbtn">
                                             <button
@@ -73,8 +73,7 @@ const Cart = () => {
                                             <span className="quantity">{item.quantity}</span>
                                             <button
                                                 className="quantity-btn"
-                                                onClick={() => handleIncreaseQuantity(item.id)}
-                                            >
+                                                onClick={() => handleIncreaseQuantity(item.id)}>
                                                 +
                                             </button>
                                         </div>
@@ -84,8 +83,7 @@ const Cart = () => {
                                                 <h2>{item.model}</h2>
                                                 <button
                                                     className="remove-cart"
-                                                    onClick={() => dispatch(RemoveItem({ id: item.id }))}
-                                                >
+                                                    onClick={() => dispatch(RemoveItem({ id: item.id }))} >
                                                     Remove Cart
                                                 </button>
                                             </span>
@@ -112,7 +110,8 @@ const Cart = () => {
                 <button className="gobackaddcart" onClick={() => Navi(-1)}>
                     Go Back
                 </button>
-            </div>
+            </div><br/><br/><br/>
+            <Footer/>
         </>
     );
 };
