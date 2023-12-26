da68118312d79a753771616ab30a933c48b1bfe5
Rohit Yadav <131008606+rohit-yadav-4099@users.noreply.github.com>
2023-12-26 15:48:02 +0530

    Update Men.js

--- src/Component/Men.js
+++ src/Component/Men.js
@@ -12,7 +12,7 @@ function PearCompo() {
 
     useEffect(() => {
         async function fetchapi() {
-            const ffdata = await fetch("https://ecomweb-c8m3.onrender.com/api/getdata");
+            const ffdata = await fetch("https://ecombackend-aiqz.onrender.com/api/getdata");
             const res = await ffdata.json();
 
             setData(res);
