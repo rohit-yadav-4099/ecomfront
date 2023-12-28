38c6e32b8c696055ccb8c78f958e881cc70ddb4b
Rohit Yadav <131008606+rohit-yadav-4099@users.noreply.github.com>
2023-12-28 11:43:54 +0530

    Sucess-ho-gya

--- src/Redux/Sucess.js
+++ src/Redux/Sucess.js
@@ -5,11 +5,14 @@ function Success() {
   const Navi = useNavigate();
   return (
     <>
-    <div className="paymentpage">
-      <div className="sucesss">Payment Success</div>
-      <div className="gobackpayment_div">
-      <button onClick={() => Navi("/")} className="gobackpayment">go to home page</button>
-      </div>
+      <div className="paymentpage">
+        <div className="sucessgif">
+          <img src="./imagepng/succes.gif" alt="success" ></img>
+        </div>
+        <div className="sucesss">Payment Success</div>
+        <div className="gobackpayment_div">
+          <button onClick={() => Navi("/")} className="gobackpayment">go to home page</button>
+        </div>
       </div>
     </>
   );
